b85b49897dbb035ae817af89d96357e415fece62
Koichi Hayashida <koichi-hayashida@cookpad.com>
2014-10-23 09:30:54 +0900

    Initial Commit

---
+++ YourVoiceAlarm/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  YourVoiceAlarm
+//
+//  Created by  koichi-hayashida on 2014/10/23.
+//  Copyright (c) 2014年  koichi-hayashida. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ YourVoiceAlarm/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  YourVoiceAlarm
+//
+//  Created by  koichi-hayashida on 2014/10/23.
+//  Copyright (c) 2014年  koichi-hayashida. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
